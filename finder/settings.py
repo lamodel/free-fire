382ba074892428a7e7fe45fe2253a7637a90608e
lamodel <lamosocial13@gmail.com>
2021-05-26 13:22:38 +0530

    commit

--- finder/settings.py
+++ finder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+wantqzb&6m!tg*mlx8&dqj(kjabx^3g#6)um54&t69w=%#&97
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','freediamondsevent.herokuapp.com']
 
 
 # Application definition
